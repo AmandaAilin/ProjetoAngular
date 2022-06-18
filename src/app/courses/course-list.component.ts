18ce3409305d4f635e194a3825f7dce364a2f621
Amanda Ailin <amandaailindev@gmail.com>
2022-06-17 22:08:26 -0300

    Eventos data binding

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,11 +8,27 @@ import { CourseService } from "./course.service";
 })
 export class CourseListComponent implements OnInit {
 
-    courses: Course[] = [];
+    filteredCourse: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy: string;
     
     constructor(private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courses = this.courseService.retrieveAll();
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourse = this._courses;
     }
+    
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this.filteredCourse = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
+    }
+
 }
