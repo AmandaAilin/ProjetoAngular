12bc080092283cccb4fdd28497d053b2f9d1f0c7
Amanda Ailin <amandaailindev@gmail.com>
2022-06-18 00:04:49 -0300

    Trabalhando com Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Erro404Component } from './erro-404/erro-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
